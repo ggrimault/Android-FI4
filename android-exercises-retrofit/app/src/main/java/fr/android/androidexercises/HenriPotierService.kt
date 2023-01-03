9621d4439aa83458f12f83e042d9ff0e652bdf96
yellowColor <guillaume.grimault64@orange.fr>
2023-01-03 11:31:27 +0100

    ajout API - initialisation

--- android-exercises-retrofit/app/src/main/java/fr/android/androidexercises/HenriPotierService.kt
+++ android-exercises-retrofit/app/src/main/java/fr/android/androidexercises/HenriPotierService.kt
@@ -2,4 +2,6 @@ package fr.android.androidexercises
 
 interface HenriPotierService {
     // TODO Method GET books which return a List<Book>
+    //Use the following URL: https://henri-potier.xebia.fr/books with retrofit to get the list of books
+    //https://www.section.io/engineering-education/making-api-requests-using-retrofit-android/
 }
